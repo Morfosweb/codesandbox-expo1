c1ed22aa82be4393f2d10a1a3b17a2126aee628f
Juan Diego <juandiegosg@gmail.com>
2020-01-26 19:07:08 -0300

    add: expo prj

---
+++ start-expo/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>This is an Expo STARTER</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'purple',
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+});
